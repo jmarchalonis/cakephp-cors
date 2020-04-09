f20202940f15ac30218ccc65c5e7f3e8df777595
jmarchalonis <jmarchalonis@gmail.com>
2020-04-09 15:48:24 -0400

    Cakephp 4.0 Updates

--- src/Routing/Middleware/CorsMiddleware.php
+++ src/Routing/Middleware/CorsMiddleware.php
@@ -1,41 +1,51 @@
 <?php
+
 namespace Cors\Routing\Middleware;
 
 use Cake\Core\Configure;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Http\Message\Builder\ResponseBuild;
+use Cake\Http\Client\Response;
+use Exception;
+use Zend\Diactoros\Response as ZendResponse;
 
-class CorsMiddleware
+class CorsMiddleware implements MiddlewareInterface
 {
 
     /**
      * PHPCS docblock fix needed!
      */
-    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, $next) {
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
         if ($request->getHeader('Origin')) {
-            $response = $response
-                ->withHeader('Access-Control-Allow-Origin', $this->_allowOrigin($request))
-                ->withHeader('Access-Control-Allow-Credentials', $this->_allowCredentials())
-                ->withHeader('Access-Control-Max-Age', $this->_maxAge())
-                ->withHeader('Access-Control-Expose-Headers', $this->_exposeHeaders())
-            ;
-
             if (strtoupper($request->getMethod()) === 'OPTIONS') {
+                $response = new ZendResponse();
                 $response = $response
                     ->withHeader('Access-Control-Allow-Headers', $this->_allowHeaders($request))
-                    ->withHeader('Access-Control-Allow-Methods', $this->_allowMethods())
-                ;
+                    ->withHeader('Access-Control-Allow-Methods', $this->_allowMethods());;
                 return $response;
+            } else {
+                $response = $handler->handle($request);
             }
+        } else {
+            $response = $handler->handle($request);
         }
 
-        return $next($request, $response);
+        $response = $response
+            ->withHeader('Access-Control-Allow-Origin', $this->_allowOrigin($request))
+            ->withHeader('Access-Control-Allow-Credentials', $this->_allowCredentials())
+            ->withHeader('Access-Control-Max-Age', $this->_maxAge())
+            ->withHeader('Access-Control-Expose-Headers', $this->_exposeHeaders());
+
+        return $response;
     }
 
     /**
      * PHPCS docblock fix needed!
      */
-    private function _allowOrigin($request) {
+    private function _allowOrigin($request)
+    {
         $allowOrigin = Configure::read('Cors.AllowOrigin');
         $origin = $request->getHeader('Origin');
 
@@ -44,7 +54,7 @@ class CorsMiddleware
         }
 
         if (is_array($allowOrigin)) {
-            $origin = (array) $origin;
+            $origin = (array)$origin;
 
             foreach ($origin as $o) {
                 if (in_array($o, $allowOrigin)) {
@@ -61,38 +71,42 @@ class CorsMiddleware
     /**
      * PHPCS docblock fix needed!
      */
-    private function _allowCredentials() {
+    private function _allowCredentials()
+    {
         return (Configure::read('Cors.AllowCredentials')) ? 'true' : 'false';
     }
 
     /**
      * PHPCS docblock fix needed!
      */
-    private function _allowMethods() {
-        return implode(', ', (array) Configure::read('Cors.AllowMethods'));
+    private function _allowMethods()
+    {
+        return implode(', ', (array)Configure::read('Cors.AllowMethods'));
     }
 
     /**
      * PHPCS docblock fix needed!
      */
-    private function _allowHeaders($request) {
+    private function _allowHeaders($request)
+    {
         $allowHeaders = Configure::read('Cors.AllowHeaders');
 
         if ($allowHeaders === true) {
             return $request->getHeader('Access-Control-Request-Headers');
         }
 
-        return implode(', ', (array) $allowHeaders);
+        return implode(', ', (array)$allowHeaders);
     }
 
     /**
      * PHPCS docblock fix needed!
      */
-    private function _exposeHeaders() {
+    private function _exposeHeaders()
+    {
         $exposeHeaders = Configure::read('Cors.ExposeHeaders');
 
         if (is_string($exposeHeaders) || is_array($exposeHeaders)) {
-            return implode(', ', (array) $exposeHeaders);
+            return implode(', ', (array)$exposeHeaders);
         }
 
         return '';
@@ -101,8 +115,9 @@ class CorsMiddleware
     /**
      * PHPCS docblock fix needed!
      */
-    private function _maxAge() {
-        $maxAge = (string) Configure::read('Cors.MaxAge');
+    private function _maxAge()
+    {
+        $maxAge = (string)Configure::read('Cors.MaxAge');
 
         return ($maxAge) ?: '0';
     }
